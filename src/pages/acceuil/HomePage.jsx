0b1e064f2a14bb36b296274932779c1389ac8fdf
njoya-tech <junior.njoya@elecit.net>
2025-12-02 18:24:49 +0100

    first commit

--- src/pages/acceuil/HomePage.jsx
+++ src/pages/acceuil/HomePage.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
 import LanguageSwitcher from '../../components/features/LanguageSwitcher'
 import NavBar from '../../components/features/NavBar'
+import ImageWithTextOverlay from '../../components/ImageWithTextOverlay'
+import HeroSection from '../../components/HomePage/HeroSection'
+import slide1_home from '../../assets/slide1_home.jpg';
+import slide2_home from '../../assets/slide2_home.jpg';
+import slide3_home from '../../assets/slide3_home.jpg';
 const HomePage = () => {
+
+
+  const heroSlides = [
+    {
+      image: slide1_home,
+      subtitle: 'hero.subtitle',
+      title: 'hero.slide1.title',
+      highlighted: 'hero.slide1.highlighted',
+      title2: 'hero.slide1.title2',
+      highlighted2: 'hero.slide1.highlighted2',
+      title3: 'hero.slide1.title3'
+    },
+    {
+      image: slide2_home,
+      subtitle: 'hero.subtitle',
+      title: 'hero.slide2.title',
+      highlighted: 'hero.slide2.highlighted',
+      title2: 'hero.slide2.title2',
+      highlighted2: 'hero.slide2.highlighted2',
+      title3: 'hero.slide2.title3'
+    },
+    {
+      image: slide3_home,
+      subtitle: 'hero.subtitle',
+      title: 'hero.slide3.title',
+      highlighted: 'hero.slide3.highlighted',
+      title2: 'hero.slide3.title2',
+      highlighted2: 'hero.slide3.highlighted2',
+      title3: 'hero.slide3.title3'
+    }
+  ];
+
+
   return (
     <>
     
@@ -9,7 +47,18 @@ const HomePage = () => {
 
      <NavBar/>
 
+ <HeroSection slides={heroSlides} />
+
+
+
+ 
+         {/* <section className='relative w-full max-w-lg mx-auto'>
+                <div className='absolute inset'>
+
+                </div>
+         </section>
     <div className='text-green-600 font-montserrat   flex justify-center  '>HomePage</div>
+    <ImageWithTextOverlay></ImageWithTextOverlay> */}
 
     </>
   )
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,5 +1,6 @@
 export const MY_COLORS = {
     primaryBlue: '#006F95',
+    secondaryGreen: '#7DA837',
     green: '#7EA72E',
     red: '#E30613',
     black: '#00121C',
